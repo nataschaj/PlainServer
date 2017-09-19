718b62564bd659b409b3c7370d38c9675b0c6760
natsachaj <natascha_jakobsen@hotmail.com>
2017-09-19 13:08:17 +0200

    Opdateret

---
+++ ModelLib/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLib
+{
+    class Car
+    {
+        //properties 'model, color, registration number'
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string RegNo { get; set; }
+    }
+}
---
+++ PlainServer/Server.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainServer
+{
+    class Server
+    {
+        public void Start()
+        {
+            TcpListener server = new TcpListener(IPAddress.Loopback, 10001);
+
+        }
+    }
+}
